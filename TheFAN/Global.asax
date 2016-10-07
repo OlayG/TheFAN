ed0c1ac6edbe7f3d9e44c67f5e6c577742d8a284
OlayG <ogureje@yahoo.com>
2016-10-06 21:21:59 -0400

    Add project files.

---
+++ TheFAN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheFAN.MvcApplication" Language="C#" %>
---
+++ TheFAN/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheFAN.Startup))]
+namespace TheFAN
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
